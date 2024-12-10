231da6d5231cd4654000cbe6c75371a1378bf1c0
Arkadiusz <arkadiusz.r.wojciechowski@gmail.com>
2024-12-10 21:18:13 +0100

    Create a Next.js Application

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Hello from "./compontents/hello";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to next.js</h1>
+      <Hello />
+    </div>
+  );
+}
